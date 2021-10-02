8b2df1e99bb5547ce9518296e2e88581f588da00
shareDreams <17240009@leon.tecnm.mx>
2021-10-02 15:45:24 -0500

    assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,34 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## A function that returns a list containing 
+## the function to save the inverse of a matrix
+## and the original matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(m){
+    x <<- m
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set=set,get=get,setsolve = setsolve, getsolve=getsolve)
 }
 
 
 ## Write a short comment describing this function
-
+## A function that inverse a matrix
+## fisrt checks if the matrix has not been inversed
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      s <- x$getsolve()
+      if(!is.null(s)){
+          message("inversed matrix")
+          return(s)
+      }
+      data <- x$get()
+      s <- solve(data,...)
+      x$setsolve(s)
+      s
 }
